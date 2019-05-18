3dab298e8a7251e62dc767f34839b0e854f0fd6c
Evgeniya Levanova <maugin666@gmail.com>
2019-05-19 04:13:55 +0500

    add comment

--- front/src/blocks/Comments/Comments.js
+++ front/src/blocks/Comments/Comments.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import CommentForm from '../../components/CommentForm/CommentForm';
+import Comment from '../../components/Comment/Comment';
+
+const comments = [
+  {
+    "id": 1,
+    "author": "Весемир",
+    "url": "",
+    "text": "Недавно знакомый рассказал, что помимо того, что в его дворе мусорные баки заполнены уже целых 2 недели, вокруг мусорных баков очень много мусора. На фотографиях можете увидеть сколько мусора там скопилось. Предлагаю встретиться и помочь жителям этого двора.",
+    "date": "Sun May 18 2019 03:53:17 GMT+0500"
+  },
+  {
+    "id": 2,
+    "author": "Весемир",
+    "url": "",
+    "text": "Недавно знакомый рассказал, что помимо того, что в его дворе мусорные баки заполнены уже целых 2 недели, вокруг мусорных баков очень много мусора. На фотографиях можете увидеть сколько мусора там скопилось. Предлагаю встретиться и помочь жителям этого двора.",
+    "date": "Sun May 18 2019 03:53:17 GMT+0500"
+  }
+];
 
 export default class Comments extends React.Component {
   render() {
@@ -9,6 +27,7 @@ export default class Comments extends React.Component {
       <div className="comments">
         <div className="comments-title">Все комментарии</div>
         <CommentForm />
+        {comments.length && comments.map(el => <Comment data={el} key={el.id}/>)}
       </div>
     );
   }
---
+++ front/src/components/Comment/Comment.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Comment = props => {
+  const {id, author, url, text, date} = props.data;
+  const messageDate = new Date(date);
+  const today = new Date();
+  const day = messageDate.toLocaleString('ru-RU', { month: 'long', day: 'numeric' });
+  const minutes = messageDate.getMinutes();
+  const messageMinutes = minutes.toString().length === 1 ? `0${minutes}` : minutes;
+  const time = `${messageDate.getHours()}:${messageMinutes}`;
+
+  return (
+    <div className="comment" id={id}>
+      <div className="comment-author">
+        <img className="comment-avatar" url={url} alt="Аватар"/>
+        <div className="comment-name">{author}</div>
+        <div className="comment-date">{today.getUTCDate() === messageDate.getUTCDate() ? time : day}</div>
+      </div>
+      <div className="comment-text">{text}</div>
+    </div>
+  );
+};
+
+export default Comment;
